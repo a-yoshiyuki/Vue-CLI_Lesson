f6c57af1a52d8284b1256a937a77a4d21adcf0c2
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-06-17 02:00:49 +0000

    Lesson-127

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <keep-alive>
       <component :is="currentComponent"></component>
     </keep-alive>
-    <div>
+    <div style="padding: 10rem;">
       <h2>イベントのフォーム</h2>
       <label for="title">タイトル</label>
       <input
@@ -40,6 +40,59 @@
         v-model="eventData.detail"
       ></textarea>
       <p style="white-space: pre;">{{ eventData.detail }}</p>
+      <input
+        type="checkbox"
+        id="isPrivate"
+        v-model="eventData.isPrivate"
+        />
+      <label for="isPrivate">非公開</label>
+      <p>{{ eventData.isPrivate }}</p>
+      <p>参加条件</p>
+      <input
+        type="checkbox"
+        id="10"
+        value="10代"
+        v-model="eventData.target"
+      />
+      <label for="10">10代</label>
+      <input
+        type="checkbox"
+        id="20"
+        value="20代"
+        v-model="eventData.target"
+      />
+      <label for="20">20代</label>
+      <input
+        type="checkbox"
+        id="30"
+        value="30代"
+        v-model="eventData.target"
+      />
+      <label for="30">30代</label>
+      <p>{{ eventData.target }}</p>
+      <P>参加費</P>
+      <input
+        type="radio"
+        id="free"
+        value="無料"
+        v-model="eventData.price"
+      />
+      <label for="free">無料</label>
+      <input
+        type="radio"
+        id="paid"
+        value="有料"
+        v-model="eventData.price"
+      />
+      <label for="paid">有料</label>
+      <p>開催場所</p>
+      <select v-model="eventData.location" multiple>
+        <option
+          v-for="location in locations"
+          :key="location"
+        >{{ location }}</option>
+      </select>
+      <P>{{ eventData.location }}</P>
     </div>
   </div>
 </template>
@@ -54,11 +107,16 @@ import Home from "./components/Home.vue";
       return {
         number: 14,
         currentComponent: "Home",
+        locations: ["東京", "大阪", "名古屋"],
         eventData: {
           title: "タイトル",
           maxNumber: 0,
           host: "",
-          detail: ""
+          detail: "",
+          isPrivate: false,
+          target: [],
+          price: "無料",
+          location: []
         }
       };
     },
